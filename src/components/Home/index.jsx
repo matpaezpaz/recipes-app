7750fb2dce0e43ee06379ba7fe4e5873050452f1
Matías <matpaezpaz@gmail.com>
2019-06-03 11:54:46 -0300

    Recipe section refactored

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
-
 import RecipeSection from '../RecipeSection';
-
 import './style.scss';
 import ChefSection from '../ChefSection';
+import { getRecipes } from '../../redux/actions';
+import { connect } from 'react-redux';
+
+const mapStateToProps = (state) => ({
+    loading: state.recipes.isRecipesLoading === true,
+    recipes: state.recipes.recipes
+});
+
+
 
 class Home extends React.Component {
+    componentDidMount() {
+        this.props.dispatch(getRecipes());
+    }
+
+
     render() {
         return (
             <div className="container">
@@ -15,7 +27,7 @@ class Home extends React.Component {
                             Latest Recipes
                         </h1>
                     </header>
-                    <RecipeSection />
+                    <RecipeSection recipes={this.props.recipes} loading={this.props.loading}/>
                 </section>
                 <section className="home-section">
                     <header className="home-section__header">
@@ -30,4 +42,4 @@ class Home extends React.Component {
     }
 }
 
-export default Home;
+export default connect(mapStateToProps,null)(Home);
--- src/components/RecipeSection/index.jsx
+++ src/components/RecipeSection/index.jsx
@@ -1,29 +1,16 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux';
+import React from 'react'
 import './style.scss';
 import Loading from '../Loading';
 import CardRecipe from '../CardRecipe';
-import { getRecipes } from '../../redux/actions';
-
-const mapStateToProps = (state) => ({
-    loading: state.recipes.isRecipesLoading === true,
-    recipes: state.recipes.recipes
-});
-
-class RecipeSection extends Component {
-    componentDidMount() {
-        this.props.dispatch(getRecipes());
-    }
-    render() {
-        return (
-            <div className="recipe-section">
-                {
-                    this.props.loading ? (<Loading />) :
-                        this.props.recipes ? this.props.recipes.map(recipe => <CardRecipe key={recipe.id} {...recipe} />) : 'No recipes'
-                }
-            </div>
-        )
-    }
+const RecipeSection = ({recipes,loading}) => {
+    return (
+        <div className="recipe-section">
+            {
+                loading ? (<Loading />) :
+                    recipes ? recipes.map(recipe => <CardRecipe key={recipe.id} {...recipe} />) : 'No recipes'
+            }
+        </div>
+    )
 }
 
-export default connect(mapStateToProps,null)(RecipeSection);
+export default RecipeSection;
